31789631e88a394ed7da5194172767a8ed49160c
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-09-18 23:07:22 +0300

    add codewars level6 and level8

--- src/CodeWars/Level6.java
+++ src/CodeWars/Level6.java
@@ -35,6 +35,29 @@ It will never give you an empty array (that's not a walk, that's standing still!
         return false;
     }
 
+    /*
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+which is the number of times you must multiply the digits in num until you reach a single digit.
+For example (Input --> Output):
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+     */
+
+    public static int persistence(long n) {
+        String str = Long.toString(n);
+        char[] array = str.toCharArray();
+        int count = 0;
+        while (array.length > 1){
+            int mult = 1;
+            for (int i = 0; i < array.length; i++) {
+                int num = Character.getNumericValue(array[i]);
+                mult = mult * num;
+            }
+            count +=1;
+            str = Integer.toString(mult);
+            array = str.toCharArray();
+        }
+        return count;
+    }
 
 
     public static void main(String[] args) {
@@ -43,6 +66,9 @@ It will never give you an empty array (that's not a walk, that's standing still!
 //        System.out.println(isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}));
 //        System.out.println(isValid(new char[] {'w'}));
         System.out.println(isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
+
+        System.out.println(persistence(999));
+
 
 
 
