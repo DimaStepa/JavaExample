1b27acd6f501b7edfda6774d6382449a8909fa09
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-09-21 23:43:35 +0300

    upd codewars level7

--- src/CodeWars/Level7.java
+++ src/CodeWars/Level7.java
@@ -88,16 +88,65 @@ String will never be empty and you do not need to account for different data typ
     }
 
 
+/*
+In this little assignment you are given a string of space separated
+numbers, and have to return the highest and lowest number.
+ */
+
+    public static String highAndLow(String numbers) {
+        String[] strArray = numbers.split(" ");
+        int temp;
+        int min = Integer.parseInt(strArray[0]);
+        int max = Integer.parseInt(strArray[strArray.length-1]);
+        if (min > max) {
+            temp = max;
+            max = min;
+            min = temp;
+        }
+        for (int i = 1; i < strArray.length; i++) {
+            if( min > Integer.parseInt(strArray[i])){
+                min = Integer.parseInt(strArray[i]);
+            } else if (max < Integer.parseInt(strArray[i])) {
+                max = Integer.parseInt(strArray[i]);
+            }
+        }
+        String result = max + " " + min;
+        return result;
+    }
+
+/*
+Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.
+ */
+
+    public static int getCount(String str) {
+        String[] countNumber = str.split("");
+        int count = 0;
+        for (int i = 0; i < countNumber.length; i++) {
+            if (countNumber[i].equals("a") || countNumber[i].equals("e")
+                    ||countNumber[i].equals("i") || countNumber[i].equals("o")
+                    || countNumber[i].equals("u")){
+                count +=1;
+            }
+        }
+        return count;
+    }
+
+
+
     public static void main(String[] args) {
 
-        System.out.println(makeComplement("TAACG"));
+//        System.out.println(makeComplement("TAACG"));
+//
+//        System.out.println(accum("NyffsGeyylB"));
+//
+//        System.out.println(ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(1,1,1,1))));
+//
+//        System.out.println(findShort("I love you"));
 
-        System.out.println(accum("NyffsGeyylB"));
-
-        System.out.println(ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(1,1,1,1))));
-
-        System.out.println(findShort("I love you"));
-
+        System.out.println(highAndLow("100 3 -5 42 -8"));
+        System.out.println(getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"));
     }
 
 }
