5cd9a158455739f6bc3b631105144b81b6ca977a
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-10-18 21:43:44 +0300

    upd HW20

--- src/RedRover/M20_HW/M20_HW.java
+++ src/RedRover/M20_HW/M20_HW.java
@@ -7,8 +7,10 @@ public class M20_HW {
     public static void main(String[] args) {
 
         List<String> list = List.of("каждый", "охотник");
-        System.out.println(UtilsHW.listToChars(list));
-
+        System.out.println(UtilsHW.flatStringList(list));
+        System.out.println(UtilsHW.flatStringListStream(list));
+//        System.out.println(UtilsHW.flatStringList(List.of(List.of(1,2,3), List.of(4,5,6)), String::valueOf));
+        System.out.println("=======");
         List<Integer> listInt = List.of(3, 0, -4, 2);
         System.out.println(UtilsHW.flattenInts(listInt));
 
--- src/RedRover/M20_HW/UtilsHW.java
+++ src/RedRover/M20_HW/UtilsHW.java
@@ -2,32 +2,38 @@ package RedRover.M20_HW;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 public class UtilsHW {
 
-    public static <T> List<T> moveList (List<T> list, TransformHW<T> transformValue){
-        List<T> result = new ArrayList<>();
-        for (T currentElement: list) {
-            T elementAfterTransformation = transformValue.transformOriginal(currentElement);
-            result.add(elementAfterTransformation);
+    public static <E, R, T extends Iterable<E>> List<R> flatStringList(List<T> list, Function<E, R> transformValue) {
+        List<R> result = new ArrayList<>();
+        for (T iterable : list) {
+            for (E element : iterable) {
+                result.add(transformValue.apply(element));
+            }
         }
-//        List <T> wordStream = result
-//                .stream()
-//                .flatMapToInt(line -> line.getClass().getResourceAsStream())
-//                .collect(Collectors.toList());
         return result;
     }
 
-    public static List<String> listToChars (List<String> source){
-        return moveList(
-                source, original -> {
-                    List<Character> chars = new ArrayList<>();
-                    for (char ch: original.toCharArray()) {
-                        chars.add(ch);
-                    }
-                    return chars.toString();
-                });
+    public static List<String> flatStringList(List<String> source) {
+        List<String> result = new ArrayList<>();
+        for (String str : source) {
+            for (int i = 0; i < str.length(); i++) {
+                result.add(String.valueOf(str.charAt(i)));
+            }
+        }
+        return result;
+    }
+
+    public static List<String> flatStringListStream(List<String> source) {
+        return source.stream()
+                // исходными данными для выполнения операции является результат предыдущей операции
+                .flatMapToInt(String::codePoints) // перебор каждого элемента source и получение int stream
+                .mapToObj(i -> String.valueOf((char) i)) // возвращаем stream String'ов
+                .collect(Collectors.toList()); //собрать все что есть в стриме и выдать список
     }
 
 //    public static <T> List<T> filterListInteger (List<T> source, List<T> checker){
@@ -40,11 +46,11 @@ public class UtilsHW {
 //        return result;
 //    }
 
-    public static List<Integer> flattenInts(List<Integer>source){
+    public static List<Integer> flattenInts(List<Integer> source) {
         List<Integer> checker = new ArrayList<>();
-        for (Integer newElement: source) {
+        for (Integer newElement : source) {
             if (newElement > 0) {
-                for (int i = 1; i < newElement+1; i++) {
+                for (int i = 1; i < newElement + 1; i++) {
                     checker.add(i);
                 }
             } else if (newElement < 0) {
@@ -55,9 +61,4 @@ public class UtilsHW {
         }
         return checker;
     }
-
-
-
-
-
 }
