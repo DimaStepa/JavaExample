319e73e2a082ec2fe946c933a680005d6642391f
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-11-04 22:26:50 +0300

    upd l25

--- src/RedRover/M25_L/Main.java
+++ src/RedRover/M25_L/Main.java
@@ -2,23 +2,22 @@ package RedRover.M25_L;
 
 public class Main {
 
-    public static void grade (int value) {
+    public static void grade(int value) throws MyException {
         if (value > 2 && value < 5) {
             System.out.println("ololo");
-        }
-        else {
-            throw new IllegalArgumentException("Grade can be only between 2 and 5");
+        } else {
+            throw new MyException("Grade can be only between 2 and 5");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             Main.grade(6);
-        }
-        catch (IllegalArgumentException e) {
+
+        } catch (MyException e) {
             System.out.println("Tut exception");
+            e.printStackTrace();  // напечатать ексэптион
         }
         Main.grade(3);
     }
-
 }
---
+++ src/RedRover/M25_L/MyException.java
@@ -0,0 +1,5 @@
+package RedRover.M25_L;
+
+public class MyException extends Exception{
+    public MyException(String nameError) {}
+}
