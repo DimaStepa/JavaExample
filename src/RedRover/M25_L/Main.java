f5e2fdc4ea2137b725dc417c16e6e2509bd7dba1
Дмитрий Степанов <dm.stepanov993@gmail.com>
2023-11-07 10:00:42 +0300

    upd l25

--- src/RedRover/M25_L/Main.java
+++ src/RedRover/M25_L/Main.java
@@ -10,14 +10,34 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        try {
-            Main.grade(6);
-
-        } catch (MyException e) {
-            System.out.println("Tut exception");
-            e.printStackTrace();  // напечатать ексэптион
-        }
-        Main.grade(3);
+    static void tryMe() {
+//        throw new RuntimeException();
+        System.out.println("Ok");
     }
+
+    public static void main(String[] args) throws Exception {
+//        try {
+//            Main.grade(6);
+//
+//        } catch (MyException e) {
+//            System.out.println("Tut exception");
+//            e.printStackTrace();  // напечатать ексэптион
+//        }
+//        Main.grade(3);
+
+        try {
+            System.out.println("Trying");
+            tryMe();
+            System.out.println("Tried");
+        }
+        catch (RuntimeException e) {
+            System.out.println("Caught exception");
+        }
+        finally {
+            System.out.println("Finally");
+        }
+
+
+    }
+
 }
