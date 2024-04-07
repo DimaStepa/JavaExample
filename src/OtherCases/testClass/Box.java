36bb081994963879c43c010cb067a97f005bf3b4
Дмитрий Степанов <dm.stepanov993@gmail.com>
2024-04-07 19:51:27 +0300

    add introduction classes

--- src/OtherCases/testClass/Box.java
+++ src/OtherCases/testClass/Box.java
@@ -1,10 +1,15 @@
 package OtherCases.testClass;
 
 public class Box {
-    double depth;
-    double wight;
-    double height;
+    private double depth;
+    private double wight;
+    private double height;
 
+    void setDimension(double d, double w, double h){
+        depth = d;
+        wight = w;
+        height = h;
+    }
     double volumeBox() {
         return depth*wight*height;
     }
--- src/OtherCases/testClass/Flat.java
+++ src/OtherCases/testClass/Flat.java
@@ -4,9 +4,7 @@ public class Flat {
 
     public static void main(String[] args) {
         Box roomFirst = new Box();
-        roomFirst.wight = 2.5;
-        roomFirst.height = 4.44;
-        roomFirst.depth = 6.0;
+        roomFirst.setDimension(10.0, 44.92, 0.543);
         System.out.println("Объем комнаты: " + roomFirst.volumeBox());
     }
 
